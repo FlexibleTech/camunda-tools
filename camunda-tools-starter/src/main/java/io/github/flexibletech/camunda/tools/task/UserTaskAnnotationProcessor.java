e9c233f6950349bb80ef9a84c5eae73c719e9e40
Maksim Batischev <Maksim.Batischev@raiffeisen.ru>
2022-08-11 13:15:35 +0300

    Method checkProcessKeyValueAnnotation has been added to UserTaskAnnotationProcessor.

--- camunda-tools-starter/src/main/java/io/github/flexibletech/camunda/tools/task/UserTaskAnnotationProcessor.java
+++ camunda-tools-starter/src/main/java/io/github/flexibletech/camunda/tools/task/UserTaskAnnotationProcessor.java
@@ -1,25 +1,31 @@
 package io.github.flexibletech.camunda.tools.task;
 
 import com.google.auto.service.AutoService;
+import io.github.flexibletech.camunda.tools.common.AbstractAnnotationProcessor;
 
-import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
 
 @AutoService(Processor.class)
 @SupportedAnnotationTypes("io.github.flexibletech.camunda.tools.task.UserTask")
 @SupportedSourceVersion(SourceVersion.RELEASE_11)
-public class UserTaskAnnotationProcessor extends AbstractProcessor {
+public class UserTaskAnnotationProcessor extends AbstractAnnotationProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
-    }
+        for (TypeElement annotation : annotations) {
+            Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
 
+            boolean isProcessKeyValueExists = checkProcessKeyValueAnnotation(annotatedElements);
+            if (!isProcessKeyValueExists) return false;
+        }
+        return true;
+    }
 
 }
